fcf0b05b6533a5e8a3fd2220b4150d206f7dd1e2
invoker0825 <jameszhang950825@gmail.com>
2024-04-15 06:47:31 +0200

    add block country

--- index.js
+++ index.js
@@ -30,6 +30,32 @@ app.get('/price', (req, res) => {
         .catch(err => console.error(err));
 });
 
+app.get('/block-countries', (req, res) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            authorization: 'Bearer fd4227da24b4d41edf4218a9cc8d54a8d32ee44ff716483dee0ee162b08201cf'
+        }
+    };
+
+    fetch(
+        'https://api.webflow.com/v2/collections/661cac6befc1a1ed7fdf1771/items',
+        options
+    )
+        .then(response => response.json())
+        .then(response => {
+            let result = [];
+            response
+                .items
+                .forEach(item => {
+                    result.push(item.fieldData);
+                });
+            res.send(result);
+        })
+        .catch(err => console.error(err));
+});
+
 app.listen(3000, () => {
     console.log('Proxy server is running on port 3000.');
 });
