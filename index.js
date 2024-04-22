de9697f0a650393399d892dc1640491aadb8cd54
invoker0825 <jameszhang950825@gmail.com>
2024-04-22 15:20:37 +0200

    fare update

--- index.js
+++ index.js
@@ -56,6 +56,37 @@ app.get('/block-countries', (req, res) => {
         .catch(err => console.error(err));
 });
 
+let bodyData = {
+    JourneyTypeID: 1,
+    pickUpLocationType: "AirPort",
+    pickUpLocation: "Heathrow Terminal 1 TW6 1JS",
+    DropoffLocationType: "Address",
+    DropoffLocation: "South Hill Avenue HA2 0DU",
+    PickupDate: "26/04/2016",
+    PickupTimeHr: "16",
+    PickupTimeMinute: "19"
+}
+
+app.get('/aaa', (req, res) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json'
+        },
+        body: JSON.stringify(bodyData)
+    };
+
+    fetch(
+        'http://eurosofttechbookingapi.co.uk/api/jobs/GetFaresFromDispatch',
+        options
+    )
+        .then(response => response.json())
+        .then(response => {
+            res.send(response);
+        })
+        .catch(err => console.log('err-------------------', err));
+});
+
 app.listen(3000, () => {
     console.log('Proxy server is running on port 3000.');
 });
